70b6c2b84b8f82a45f537e53be9d305f543f238c
Ganesh Nukala <ganesh@mobiux.in>
2020-12-11 19:19:54 +0530

    Components Added

---
+++ components/DisplayModeContext.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+const DisplayModeContext = createContext({
+  switchMode: null,
+  setSwitchMode: null,
+});
+export default DisplayModeContext;
---
+++ public/exampleParagraph.js
@@ -0,0 +1,3 @@
+const para =
+  "Enim do sit do laborum minim nostrud ea culpa. Anim aute aliquip in proident ex veniam. Pariatur cillum et aute aliqua nisi aliqua veniam minim elit non.";
+export default para;
